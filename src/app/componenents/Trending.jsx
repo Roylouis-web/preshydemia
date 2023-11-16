f8b101ef38ea675d73afd08bc8c6bdad6999b314
Roylouis-web <powellleroy488@gmail.com>
2023-11-16 04:48:55 +0100

    All done

--- src/app/componenents/Trending.jsx
+++ src/app/componenents/Trending.jsx
@@ -1,29 +1,25 @@
-import Image from "next/image"
-import { promisify } from "util";
 import { useState, useEffect } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import Filters from "../componenents/Filters"
-import Card from "../componenents/Card";
-import { getVegetarian } from "../../../lib/api";
+import Filters from "./Filters"
+import Card from "./Card";
+import { getTrending } from "../../../lib/api";
 
 
-type State = never[] | Item[];
-
-const Veggies = () => {
-    const state: State = [];
-    const [items, setItems] = useState(state);
+const Trending = () => {
+    const [items, setItems] = useState([]);
     const [perPage, setPerPage] = useState(window.innerWidth <= 648 ? 2 : window.innerWidth <= 1024 ? 4 : 5);
 
     useEffect(() => {
-        const veggies = localStorage.getItem("veggies");
-        if (veggies) {
-            const result = JSON.parse(veggies) as Item[];
+        const trending = localStorage.getItem("trending");
+        if (trending) {
+            const result = JSON.parse(trending);
             ((async () => setItems(result))());
         } else {
             ((async () => {
-                setItems(await getVegetarian())
+                setItems(await getTrending())
             })());
+
         }
         const setNumber = () => {
             window.innerWidth <= 648 ? setPerPage(2) : window.innerWidth <= 1024 ? setPerPage(4) : setPerPage(5);
@@ -36,17 +32,17 @@ const Veggies = () => {
 
     return (
         <div className="mt-8 p-2">
-            <h1 className="text-white lg:text-3xl text-2xl">Our veggie picks</h1>
+            <h1 className="text-white lg:text-3xl text-2xl">Trending</h1>
             <div className="mt-8">
-                <Splide options={{ perPage, pagination: false, arrows: false, drag: 'free', gap: 1 }}>
+                <Splide options={{ perPage, pagination: false, arrows: false, drag: 'free', gap: 0 }}>
                     {items.map((item) => {
                         return (
                             <SplideSlide key={crypto.randomUUID()}>
-                                <Card 
+                                <Card
                                 id={item.id}
                                 image={item.image}
                                 title={item.title}
-                                />
+                                 />
                             </SplideSlide>
                         )
                     })}
@@ -56,4 +52,4 @@ const Veggies = () => {
     )
 }
 
-export default Veggies
+export default Trending
--- src/app/componenents/Veggies.jsx
+++ src/app/componenents/Veggies.jsx
@@ -1,27 +1,26 @@
+import Image from "next/image"
+import { promisify } from "util";
 import { useState, useEffect } from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import Filters from "../componenents/Filters"
-import Card from "../componenents/Card";
-import { getTrending } from "../../../lib/api";
+import Filters from "./Filters"
+import Card from "./Card";
+import { getVegetarian } from "../../../lib/api";
 
-type State = never[] | Item[];
 
-const Trending = () => {
-    const state: State = [];
+const Veggies = () => {
     const [items, setItems] = useState(state);
     const [perPage, setPerPage] = useState(window.innerWidth <= 648 ? 2 : window.innerWidth <= 1024 ? 4 : 5);
 
     useEffect(() => {
-        const trending = localStorage.getItem("trending");
-        if (trending) {
-            const result = JSON.parse(trending) as Item[];
+        const veggies = localStorage.getItem("veggies");
+        if (veggies) {
+            const result = JSON.parse(veggies);
             ((async () => setItems(result))());
         } else {
             ((async () => {
-                setItems(await getTrending())
+                setItems(await getVegetarian())
             })());
-
         }
         const setNumber = () => {
             window.innerWidth <= 648 ? setPerPage(2) : window.innerWidth <= 1024 ? setPerPage(4) : setPerPage(5);
@@ -34,17 +33,17 @@ const Trending = () => {
 
     return (
         <div className="mt-8 p-2">
-            <h1 className="text-white lg:text-3xl text-2xl">Trending</h1>
+            <h1 className="text-white lg:text-3xl text-2xl">Our veggie picks</h1>
             <div className="mt-8">
-                <Splide options={{ perPage, pagination: false, arrows: false, drag: 'free', gap: 0 }}>
+                <Splide options={{ perPage, pagination: false, arrows: false, drag: 'free', gap: 1 }}>
                     {items.map((item) => {
                         return (
                             <SplideSlide key={crypto.randomUUID()}>
-                                <Card
+                                <Card 
                                 id={item.id}
                                 image={item.image}
                                 title={item.title}
-                                 />
+                                />
                             </SplideSlide>
                         )
                     })}
@@ -54,4 +53,4 @@ const Trending = () => {
     )
 }
 
-export default Trending
+export default Veggies
