f557e96f58b9c555f09c19cd4f4b0feed2cdee1a
Roylouis-web <powellleroy488@gmail.com>
2023-11-17 01:14:59 +0100

    All done

--- src/app/dishes/error.tsx
+++ src/app/dishes/error.tsx
@@ -1,7 +1,8 @@
 'use client' // Error components must be Client Components
  
+import Link from 'next/link'
 import { useEffect } from 'react'
- 
+
 const Error = ({
   error,
   reset,
@@ -15,8 +16,9 @@ const Error = ({
   }, [error])
  
   return (
-    <div className='bg-slate-500 fixed top-32 min-h-screen z-10 w-full flex justify-center items-center text-white'>
+    <div className='bg-slate-500 fixed top-0 min-h-screen z-10 w-full flex justify-center items-center text-white flex-col gap-7'>
       <h1 className='text-2xl sm:text-3xl lg:text-4xl'>Daily limit reached, try again tomorrow!</h1>
+      <Link href="/">Go back</Link>
     </div>
   )
 }
