c0f725275cd528058268ef9da69d7ad7714fac83
Roylouis-web <powellleroy488@gmail.com>
2023-11-18 00:25:16 +0100

    All done

--- src/app/componenents/Search.tsx
+++ src/app/componenents/Search.tsx
@@ -9,7 +9,7 @@ const Search = () => {
 
   return (
     <div className="mx-auto flex absolute w-full top-36">
-      <div className="w-9/12 sm:w-2/3 mx-auto mt-6 flex">
+      <div className="w-10/12 sm:w-2/3 mx-auto mt-6 flex">
         <input type="search" name="search" id="search" placeholder="search" value={searched} className="w-full text-xl px-4 outline-none placeholder-slate-500 sm:text-2xl lg:text-3xl dark:bg-white bg-gray-900 rounded-xl text-gray-400 dark:text-black" onChange={(e) => setSearched(e.target.value)}/>
         <div className="bg-white dark:bg-black flex items-center text-[green] p-4 lg:text-3xl" onClick={() => router.push(`/dishes/searched?search=${searched}`)}>
         <FaSearch />
